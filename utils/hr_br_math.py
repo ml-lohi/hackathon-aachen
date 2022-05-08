78f4055adafcf1f14d4c49bb7bc1a5569362b80f
bamboo_user <sviatoslavs999@gmail.com>
2022-05-08 23:34:36 +0200

    main_app and hr_br_math final

--- utils/hr_br_math.py
+++ utils/hr_br_math.py
@@ -5,12 +5,33 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def calculate_br_with_fft(data, kernel_factor=20, sigma=25):
+def calculate_br_with_fft(data, kernel_factor=20, sigma=25, thr=20000):
     smoothed_data = filter_gauss(data, kernel_factor=kernel_factor, sigma=sigma)
-    bpm = signal.butter(1, [0.1, 1], "bandpass", fs=1000, output="sos")
+    bpm = signal.butter(6, [0.1, 1], "bandpass", fs=1000, output="sos")
     filtered = signal.sosfilt(bpm, smoothed_data)
     fft_x_fitlered, fft_y_filtered = fft(filtered)
+    indices = fft_x_fitlered > 0.15
+    fft_x_fitlered = fft_x_fitlered[indices]
+    fft_y_filtered = fft_y_filtered[indices]
     bpm_frequency = fft_x_fitlered[np.argmax(fft_y_filtered)]
+    print(f"Amplitude = {np.max(fft_y_filtered)}")
+    if np.max(fft_y_filtered) < thr:
+        bpm_frequency = 0
+    return 60 * bpm_frequency, filtered, fft_x_fitlered, fft_y_filtered
+
+
+def calculate_hr_with_fft(data, kernel_factor=3, sigma=25, thr=0):
+    smoothed_data = filter_gauss(data, kernel_factor=kernel_factor, sigma=sigma)
+    bpm = signal.butter(6, [0.5, 4], "bandpass", fs=1000, output="sos")
+    filtered = signal.sosfilt(bpm, smoothed_data)
+    fft_x_fitlered, fft_y_filtered = fft(filtered)
+    indices = fft_x_fitlered > 0.5
+    fft_x_fitlered = fft_x_fitlered[indices]
+    fft_y_filtered = fft_y_filtered[indices]
+    bpm_frequency = fft_x_fitlered[np.argmax(fft_y_filtered)]
+    print(f"Amplitude HR = {np.max(fft_y_filtered)}")
+    if np.max(fft_y_filtered) < thr:
+        bpm_frequency = 0
     return 60 * bpm_frequency, filtered, fft_x_fitlered, fft_y_filtered
 
 
@@ -22,6 +43,8 @@ def calculate_rates_with_peaks(
     n=10,
     fs=1000,
     to_plot=False,
+    amplitude_threshold=-100,
+    min_peaks=0,
 ):
     bandpass = signal.butter(n, bandpass_range, "bandpass", fs=fs, output="sos")
     filtered = signal.sosfilt(bandpass, data)
@@ -37,8 +60,9 @@ def calculate_rates_with_peaks(
         ax[2].plot(frequencies, fft_wave)
         ax[2].set_xlim(0, 4)
 
-    periods = np.diff(x_filtered[peaks])
+    periods = np.diff(peaks)
     period_mean, period_err = np.mean(periods), np.std(periods)
     frequency = fs / period_mean * 60
     frequency_err = 1 / period_mean**2 * period_err * fs / 60
+
     return frequency, frequency_err
