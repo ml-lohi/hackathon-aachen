b36cee98e8b366e5581eea777d19959803a953bb
Kostiantyn Lavronenko <lavronenkostya@gmail.com>
2022-05-08 14:03:11 +0200

    upload current app

--- app.py
+++ app.py
@@ -4,20 +4,29 @@ from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import tkinter as tk  
 import numpy as np
 import time
+from utils.helper import fft
+
+xdata = []
+ydata = []
 
 def process(canvas,ax):
+    x = 0
     while True:
-        plot(canvas,ax)
-        time.sleep(1)
+        xdata.append(x)
+        ydata.append(np.sin(x*np.pi))
+        fftx,ffty = fft(np.array(ydata))
+        plot(canvas,ax,xdata,ydata,fftx,ffty)
+        time.sleep(0.1)
+        x=x+0.1
 
-def plot(canvas,ax):
-    c = ['r','b','g']  # plot marker colors
-    ax.clear()         # clear axes from previous plot
-    for i in range(3):
-        theta = np.random.uniform(0,360,10)
-        r = np.random.uniform(0,1,10)
-        ax.plot(theta,r,linestyle="None",marker='o', color=c[i])
-        canvas.draw()
+def plot(canvas,axs,xdata,ydata,fftx,ffty):
+    for i, ax in enumerate(axs):
+        ax.clear()         # clear axes from previous plot
+        if i == 1:
+            ax.plot(fftx,ffty)
+        else:
+            ax.plot(xdata,ydata)
+    canvas.draw()
         
 class Application(tk.Frame):
     def __init__(self, root=None):
@@ -26,23 +35,29 @@ class Application(tk.Frame):
         self.createWidgets()
 
     def createWidgets(self):
-        fig=plt.figure(figsize=(2,2))
-        ax=fig.add_axes([0.1,0.1,0.8,0.8],polar=True)
+        fig,axs=plt.subplots(3,1,figsize=(3,3))
+        fig.tight_layout()
         canvas=FigureCanvasTkAgg(fig,master=self.root)
         canvas.get_tk_widget().grid(row=0,column=1)
         canvas.draw()
-        self.thread = threading.Thread(target=process, args=(canvas,ax))
+        self.thread = threading.Thread(target=process, args=(canvas,axs))
         self.thread.setDaemon(True)
         self.thread.start()
-        self.plotbutton=tk.Button(master=self.root, text="Quit", command=self._quit)
+        
+        self.plotbutton=tk.Button(master=self.root, text="Quit", command=self.root.quit)
         self.plotbutton.grid(row=0,column=0)
+        
+        br = 15
+        lbl = tk.Label(root, text = f"Breathing rate: {br}bpm")
+        lbl.grid()
+        
+        hr = 50
+        lbl = tk.Label(root, text = f"Heart rate: {hr}hbpm")
+        lbl.grid()
     
-    def _quit(self):
-        self.root.quit()
-
-
-root=tk.Tk()
-root.title("Welcome to GeekForGeeks")
-root.geometry('550x450')
-app=Application(root=root)
-app.mainloop()
+if "__main__" == __name__:
+    root=tk.Tk()
+    root.title("Welcome to GeekForGeeks")
+    root.geometry('750x650')
+    app=Application(root=root)
+    app.mainloop()
